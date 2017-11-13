644bc9025330b4fba972213452d560ad93589407
sarvesh <sarvesh@mactores.com>
2017-11-13 11:16:32 +0530

    updated var files

--- services/host/haproxy/variables.tf
+++ services/host/haproxy/variables.tf
@@ -2,8 +2,5 @@ variable "region"				               {}
 variable "env"					               {}
 variable "appname"			               {}
 variable "bu"					                 {}
-variable "cluster_name"               { type="list"}
-variable "az"                         { type="list"}
-variable "instance_type"              { type="list"}
 variable "key_name"                   {}
 variable "ami"                        {}
