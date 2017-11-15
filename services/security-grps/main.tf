f3917908a1e7868f38398ca606e20db2559e2ebf
sarvesh <sarvesh@mactores.com>
2017-11-15 15:46:14 +0530

    updated vpc mod

---
+++ services/security-grps/main.tf
@@ -0,0 +1,21 @@
+/*
+ ########################################################
+ Author: Sarvesh Shetty
+ Date :5th July,2017
+ description : main file to create VPC
+ ########################################################
+*/
+
+provider "aws" {
+  region     = "${var.region}"
+}
+
+ terraform {
+	  backend "s3" {}
+}
+
+module "vpc" {
+  source = "../../lib"
+  env = "${var.env}"
+  app = "${var.appname}"
+}
