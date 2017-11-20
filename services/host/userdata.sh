a81b6586504109ad0def71538ca9609e9afc95ca
sarvesh <sarvesh@mactores.com>
2017-11-20 18:37:32 +0530

    updated

--- services/host/userdata.sh
+++ services/host/userdata.sh
@@ -6,3 +6,10 @@ source ~/.bashrc
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 LOGFILE="/var/log/bootstrap.log"
 echo "$TIMESTAMP: Starting to execute UserData script" >> $LOGFILE
+sudo apt-get install python-pip -y
+sudo pip install awscli
+yum install wget -y
+TAG_NAME="Name"
+INSTANCE_ID="`wget -qO- http://instance-data/latest/meta-data/instance-id`"
+REGION="`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+TAG_VALUE="`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$TAG_NAME" --region $REGION --output=text | cut -f5`"
