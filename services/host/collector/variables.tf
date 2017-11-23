5300d2fc7f1ea3394d74af746d43079d5005bc31
sarvesh <sarvesh@mactores.com>
2017-11-23 22:40:37 +0530

    updated colelctor

--- services/host/collector/variables.tf
+++ services/host/collector/variables.tf
@@ -2,5 +2,22 @@ variable "region"				               {}
 variable "env"					               {}
 variable "appname"			               {}
 variable "bu"					                 {}
-variable "key_name"                   {}
-variable "ami"                        {}
+variable "az"                          {}
+
+
+variable "yaga_key_name"                    {}
+variable "yaga_ami"                         {}
+variable "yaga_count"                       {}
+variable "yaga_ebs_optimized"               {}
+variable "yaga_monitoring"                  {}
+variable "yaga_source_dest_check"           {}
+variable "yaga_associate_public_ip_address" {}
+variable "yaga_iam_instance_profile"        {}
+variable "yaga_root-volume_type"            {}
+variable "yaga_root-volume_size"            {}
+variable "yaga_root-volume-delete_on_termination" {}
+variable "yaga_tag-type"                    {}
+variable "yaga_tag-cluster"                 {}
+variable "yaga_tag-vpc"                     {}
+variable "yaga_tag-name"                    {}
+variable "yaga_instance_type"               {}
