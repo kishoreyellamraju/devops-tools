7bb4407bd1bb04f20ce1ac684b1ef5e0a8a1cce6
sarvesh <sarvesh@mactores.com>
2017-11-23 11:55:24 +0530

    updated qw

--- services/host/queueworker/activity/variables.tf
+++ services/host/queueworker/activity/variables.tf
@@ -2,5 +2,21 @@ variable "region"				               {}
 variable "env"					               {}
 variable "appname"			               {}
 variable "bu"					                 {}
-variable "key_name"                   {}
-variable "ami"                        {}
+variable "key_name"                    {}
+variable "ami"                         {}
+variable "count"                       {}
+variable "ebs_optimized"               {}
+variable "monitoring"                  {}
+variable "source_dest_check"           {}
+variable "associate_public_ip_address" {}
+variable "iam_instance_profile"        {}
+variable "root-volume_type"            {}
+variable "root-volume_size"            {}
+variable "root-volume-delete_on_termination" {}
+variable "tag-type"                    {}
+variable "tag-cluster"                 {}
+variable "tag-vpc"                     {}
+variable "tag-name"                    {}
+variable "security_group"              {}
+variable "instance_type"               {}
+variable "az"                          { type="list"}
